6383ea9af165c05e5d5493b8819deea7cc74b809
unknown <sam.haff.rn@gmail.com>
2024-11-04 15:34:46 +0200

    added deploy to cf ci

--- .github/actions/deploy-cf/main.js
+++ .github/actions/deploy-cf/main.js
@@ -4,6 +4,12 @@ const github = require('@actions/github');
 
 function run() {
     core.notice('Hello from js action!!!');
+
+    const dir = core.getInput('deployableDir');
+    const projectName = core.getInput('projectName');
+
+    exec.exec('npm install wrangler -y');
+    exec.exec(`npx wrangler pages deploy ${dir} --project-name ${projectName}`);
 }
 
 run();
