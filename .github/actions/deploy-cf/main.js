412d241ec93530032446e2e6c9a254ddfea579ad
unknown <sam.haff.rn@gmail.com>
2024-11-04 17:18:32 +0200

    inv

--- .github/actions/deploy-cf/main.js
+++ .github/actions/deploy-cf/main.js
@@ -11,27 +11,27 @@ async function run() {
     const apiToken = core.getInput('apiToken');
     const accountID = core.getInput('accountID');
 
-    await exec.exec('sudo apt update');
+    //await exec.exec('sudo apt update');
     //await exec.exec('sudo apt install nodejs');
-    await exec.exec('ln -s /usr/bin/nodejs /usr/local/bin/node');
+    //await exec.exec('ln -s /usr/bin/nodejs /usr/local/bin/node');
     //await exec.exec('ln -s /usr/bin/nodejs /usr/local/bin/node');
     await exec.exec('npm install wrangler -y');
     //exec.exec(`echo "CLOUDFLARE_ACCOUNT_ID=${accountID}" >> $GITHUB_ENV`);
     //exec.exec(`echo "CLOUDFLARE_API_TOKEN=${apiToken}" >> $GITHUB_ENV`);
-    await exec.exec(`npx wrangler pages deploy ${dir} --project-name ${projectName}`, [], 
+    /*await exec.exec(`npx wrangler pages deploy ${dir} --project-name ${projectName}`, [], 
         {
             env:{
                 "CLOUDFLARE_ACCOUNT_ID": accountID,
                 "CLOUDFLARE_API_TOKEN": apiToken
             }
         }
-    );
-    /*exec.exec(`CLOUDFLARE_ACCOUNT_ID=${accountID}`);
-    exec.exec(`CLOUDFLARE_API_TOKEN=${apiToken}`);
-    exec.exec('echo $CLOUDFLARE_ACCOUNT_ID');
-    exec.exec('echo $CLOUDFLARE_API_TOKEN');
+    );*/
+    await exec.exec(`CLOUDFLARE_ACCOUNT_ID=${accountID}`);
+    await exec.exec(`CLOUDFLARE_API_TOKEN=${apiToken}`);
+    await exec.exec('echo $CLOUDFLARE_ACCOUNT_ID');
+    await exec.exec('echo $CLOUDFLARE_API_TOKEN');
 
-    exec.exec(`npx wrangler pages deploy ${dir} --project-name ${projectName}`);*/
+    await exec.exec(`npx wrangler pages deploy ${dir} --project-name ${projectName}`);
 }
 
 run();
