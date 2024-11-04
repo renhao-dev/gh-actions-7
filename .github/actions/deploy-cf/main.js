d77643752ef02443ea0d3eba355b129ccefa5fc9
unknown <sam.haff.rn@gmail.com>
2024-11-04 17:34:08 +0200

    cleaning up after succesful investigation

--- .github/actions/deploy-cf/main.js
+++ .github/actions/deploy-cf/main.js
@@ -11,22 +11,8 @@ async function run() {
     const apiToken = core.getInput('apiToken');
     const accountID = core.getInput('accountID');
 
-    //await exec.exec('sudo apt update');
-    //await exec.exec('sudo apt install nodejs');
-    //await exec.exec('ln -s /usr/bin/nodejs /usr/local/bin/node');
-    //await exec.exec('ln -s /usr/bin/nodejs /usr/local/bin/node');
     await exec.exec('npm install wrangler -y');
     await exec.exec('npm install cross-env -y');
-    //exec.exec(`echo "CLOUDFLARE_ACCOUNT_ID=${accountID}" >> $GITHUB_ENV`);
-    //exec.exec(`echo "CLOUDFLARE_API_TOKEN=${apiToken}" >> $GITHUB_ENV`);
-    /*await exec.exec(`npx wrangler pages deploy ${dir} --project-name ${projectName}`, [], 
-        {
-            env:{
-                "CLOUDFLARE_ACCOUNT_ID": accountID,
-                "CLOUDFLARE_API_TOKEN": apiToken
-            }
-        }
-    );*/
 
     await exec.exec(`npx cross-env CLOUDFLARE_ACCOUNT_ID=${accountID} CLOUDFLARE_API_TOKEN=${apiToken} wrangler pages deploy ${dir} --project-name ${projectName}`);
 }
