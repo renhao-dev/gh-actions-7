d0d171906f4153c8d5e99a5906621dbcfba471bd
unknown <sam.haff.rn@gmail.com>
2024-11-04 15:39:19 +0200

    fix, envs cant't access inputs :(

--- .github/actions/deploy-cf/main.js
+++ .github/actions/deploy-cf/main.js
@@ -8,8 +8,11 @@ function run() {
     const dir = core.getInput('deployableDir');
     const projectName = core.getInput('projectName');
 
+    const apiToken = core.getInput('apiToken');
+    const accountID = core.getInput('accountID');
+
     exec.exec('npm install wrangler -y');
-    exec.exec(`npx wrangler pages deploy ${dir} --project-name ${projectName}`);
+    exec.exec(`CF_ACCOUNT_ID=${accountID} CF_API_TOKEN=${apiToken} npx wrangler pages deploy ${dir} --project-name ${projectName}`);
 }
 
 run();
